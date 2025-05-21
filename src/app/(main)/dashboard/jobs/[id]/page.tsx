06f6bc06433bc826327382d8d64d717bc8a2da64
Cumstein <kamyabaghaz@gmail.com>
2025-05-21 20:23:45 +0330

    params promise fix deployment

--- src/app/(main)/dashboard/jobs/[id]/page.tsx
+++ src/app/(main)/dashboard/jobs/[id]/page.tsx
@@ -2,8 +2,9 @@
 import JobDetailsWrapper from "@/components//jobs/job-details-wrapper";
 import { getJobById } from "@/lib/jobs";
 
-export default async function JobDetailsPage({ params }: { params: { id: string } }) {
-  const job = await getJobById(params.id);
+export default async function JobDetailsPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const job = await getJobById(id);
   if (!job) return <div>Job not found.</div>;
 
   return <JobDetailsWrapper job={job} />;
